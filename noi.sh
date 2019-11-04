46246113f1d13c448bb4a6ad6520b8eb4b5e8561
David Vance <datvance@gmail.com>
2019-11-04 02:27:16 +0000

    use project dir in noish

--- noi.sh
+++ noi.sh
@@ -6,14 +6,15 @@
 #should change this to you
 github_username=datvance
 
+#maybe flimsy, could just hard-code it
 project_dir="$( cd "$( dirname "$0" )" && pwd )"
 
-php ./build.php;
+php ${project_dir}/build.php;
 
-changes=$(git status --short ./docs/index.html);
+changes=$(git status --short ${project_dir}/docs/index.html);
 
 if [ -n "$changes" ]; then
 	timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
-	git commit ./docs/index.html -m "Build ${timestamp}"
+	git commit ${project_dir}/docs/index.html -m "Build ${timestamp}"
 	git push https://${NOINSTAGRAM_GITHUB_TOKEN}:x-oauth-basic@github.com/${github_username}/noinstagram.git master
 fi
